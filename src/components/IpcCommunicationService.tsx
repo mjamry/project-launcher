2ae8f5b09e1c8dea7ff7c427540be84ce3c44dcd
mjamry <michal.jamry@gmail.com>
2022-05-26 13:57:52 +0200

    Added projects state along with IpcService to communicate with host

---
+++ src/components/IpcCommunicationService.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { Project } from '../common/dto/ProjectDto';
+import projectsState from '../state/ProjectState';
+
+const { ipcRenderer } = window.require('electron');
+
+function IpcCommunicationService() {
+  const setProjects = useSetRecoilState(projectsState);
+
+  useEffect(() => {
+    ipcRenderer.on('test', (event: any, data: Project[]) => {
+      setProjects(data);
+    });
+  }, [setProjects]);
+
+  return (
+    <></>
+  );
+}
+
+export default IpcCommunicationService;
