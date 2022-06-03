03acb04b1b0a0591c12ebfee8c7a20eba145986c
mjamry <michal.jamry@gmail.com>
2022-06-03 14:36:26 +0200

    Code cleanup

--- src/app/services/IpcCommunicationService.tsx
+++ src/app/services/IpcCommunicationService.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { ipcRenderer } from 'electron';
 import { Project } from '../../../shared/dto/ProjectDto';
 import projectsState from '../state/ProjectState';
-
-const { ipcRenderer } = window.require('electron');
 
 function IpcCommunicationService() {
   const setProjects = useSetRecoilState(projectsState);
