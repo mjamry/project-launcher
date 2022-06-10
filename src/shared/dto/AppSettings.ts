9d0aa7a8a605e8dd0a17e1088760edd0d3f23ef8
mjamry <michal.jamry@gmail.com>
2022-06-10 10:50:22 +0200

    Extended app settings

--- src/shared/dto/AppSettings.ts
+++ src/shared/dto/AppSettings.ts
@@ -2,10 +2,14 @@ export type AppSettings = {
   isDevelopment: boolean;
   jiraUrl: string;
   jiraToken: string;
+  jiraRefreshTimeoutInMinutes: number;
+  jiraChangelogFields: string[];
 };
 
 export const DefaultAppSettings: AppSettings = {
   isDevelopment: false,
   jiraUrl: '',
   jiraToken: '',
+  jiraRefreshTimeoutInMinutes: 1,
+  jiraChangelogFields: ['assignee', 'status', 'priority', 'type'],
 };
