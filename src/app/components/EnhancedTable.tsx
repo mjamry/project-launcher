846bfa2d6bfbd65adcbbcb72d427315d44f0237f
mjamry <michal.jamry@gmail.com>
2022-12-02 16:13:05 +0100

    Added sorting into table

---
+++ src/app/components/EnhancedTable.tsx
@@ -0,0 +1,169 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TablePagination,
+  Box,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  TableBody,
+} from '@mui/material';
+import React, { ReactElement } from 'react';
+import { visuallyHidden } from '@mui/utils';
+
+// type JiraItemDataDTO = {
+//   id: string,
+//   summary: string,
+//   assignee: string,
+//   status: string,
+//   updated: Date,
+//   priority: string,
+// };
+
+function descendingComparator(a: any, b: any, orderBy: keyof any) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+type Align = 'right' | 'left';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+    a: { [key in Key]: string | Date },
+    b: { [key in Key]: string | Date },
+  ) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof any;
+  label: string;
+  align?: Align;
+}
+
+interface EnhancedTableHeadProps {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof any) => void;
+  order: Order;
+  orderBy: string;
+  headCells: HeadCell[]
+}
+
+function EnhancedTableHead(props: EnhancedTableHeadProps) {
+  const {
+    order, orderBy, onRequestSort, headCells,
+  } = props;
+  // eslint-disable-next-line max-len
+  const createSortHandler = (property: keyof any) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell />
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id as string}
+            align={headCell.align ? headCell.align : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+type Props = {
+  data: any[];
+  children?: ReactElement;
+  headCells: HeadCell[];
+};
+
+function EnhancedTable(props: Props) {
+  const { data, children, headCells } = props;
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<any>('id');
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof any,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <EnhancedTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            headCells={headCells}
+          />
+          <TableBody>
+            {React.cloneElement(
+              children as React.ReactElement,
+              data
+                .sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
+}
+
+export default EnhancedTable;
--- src/app/components/JiraItems.tsx
+++ src/app/components/JiraItems.tsx
@@ -1,8 +1,18 @@
 import {
-  TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TablePagination,
+  Box,
+  TableSortLabel,
 } from '@mui/material';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
+import { visuallyHidden } from '@mui/utils';
 import { jiraHistoryState, jiraUpdatesState } from '../state/JiraState';
 import JiraUpdateItem from './JiraItemDetails';
 import { ProjectCard } from './ProjectStyledComponents';
@@ -11,12 +21,158 @@ type Props = {
   projectKey: string;
 };
 
+type JiraItemDataDTO = {
+  id: string,
+  summary: string,
+  assignee: string,
+  status: string,
+  updated: Date,
+  priority: string,
+};
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+type Align = 'right' | 'left';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+    a: { [key in Key]: string | Date },
+    b: { [key in Key]: string | Date },
+  ) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof JiraItemDataDTO;
+  label: string;
+  align?: Align;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'id',
+    disablePadding: true,
+    label: 'Id',
+  },
+  {
+    id: 'summary',
+    disablePadding: false,
+    label: 'Summary',
+  },
+  {
+    id: 'status',
+    disablePadding: false,
+    label: 'Status',
+    align: 'right',
+  },
+  {
+    id: 'priority',
+    disablePadding: false,
+    label: 'Priority',
+    align: 'right',
+  },
+  {
+    id: 'updated',
+    disablePadding: false,
+    label: 'Updated',
+    align: 'right',
+  },
+  {
+    id: 'assignee',
+    disablePadding: false,
+    label: 'Assignee',
+    align: 'right',
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof JiraItemDataDTO) => void;
+  order: Order;
+  orderBy: string;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const {
+    order, orderBy, onRequestSort,
+  } = props;
+  // eslint-disable-next-line max-len
+  const createSortHandler = (property: keyof JiraItemDataDTO) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell />
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.align ? headCell.align : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
 function JiraUpdates(props: Props) {
   const { projectKey } = props;
   const issues = useRecoilValue(jiraHistoryState)
     .find((u) => u.project === projectKey)?.issues;
   const updatedIssues = useRecoilValue(jiraUpdatesState)
     .find((u) => u.project === projectKey)?.issues;
+
+  // enhancedTable
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof JiraItemDataDTO>('id');
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof JiraItemDataDTO,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
   return (
     <>
@@ -25,29 +181,35 @@ function JiraUpdates(props: Props) {
       <ProjectCard>
         <TableContainer component={Paper}>
           <Table aria-label="collapsible table">
-            <TableHead>
-              <TableRow>
-                <TableCell />
-                <TableCell>Issue Id</TableCell>
-                <TableCell>Summary</TableCell>
-                <TableCell align="right">Status</TableCell>
-                <TableCell align="right">Priority</TableCell>
-                <TableCell align="right">Updated</TableCell>
-                <TableCell align="right">Assignee</TableCell>
-              </TableRow>
-            </TableHead>
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
             <TableBody>
-              {issues.map((issue) => (
-                <JiraUpdateItem
-                  item={issue}
-                  projectKey={projectKey}
-                  key={issue.id}
-                  updated={updatedIssues?.find((i) => i.id === issue.id) !== undefined}
-                />
-              ))}
+              {[...issues]
+                .sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((issue) => (
+                  <JiraUpdateItem
+                    item={issue}
+                    projectKey={projectKey}
+                    key={issue.id}
+                    updated={updatedIssues?.find((i) => i.id === issue.id) !== undefined}
+                  />
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={issues.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </ProjectCard>
       )}
     </>
