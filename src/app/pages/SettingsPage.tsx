f78e488635ff9d28559ed0d1ba80e4fb07652cf6
mjamry <michal.jamry@gmail.com>
2023-07-19 09:35:03 +0200

    #28 Added new component to create an empty project config file

--- src/app/pages/SettingsPage.tsx
+++ src/app/pages/SettingsPage.tsx
@@ -7,6 +7,7 @@ import { useRecoilState } from 'recoil';
 import { appSettingsState } from '../state/AppState';
 import { projectsState } from '../state/ProjectState';
 import useSettingsFileWriterService from '../services/SettingsFileWriterService';
+import CreateProjectConfig from '../components/CreateProjectConfig';
 
 const Root = styled('div')({
   margin: '10px',
@@ -154,6 +155,7 @@ function SettingsPage() {
           </ButtonContainer>
         </StyledAccordion>
       ))}
+      <CreateProjectConfig />
     </Root>
   );
 }
