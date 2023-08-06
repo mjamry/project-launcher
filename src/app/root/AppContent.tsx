366ec6723b57a8bae00c1daaea59591b37b43c74
mjamry <michal.jamry@gmail.com>
2023-08-06 14:35:08 +0200

    Fixed issue with incorrect theme props

--- src/app/root/AppContent.tsx
+++ src/app/root/AppContent.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import RouteTypes from '../common/dto/RouteTypes';
